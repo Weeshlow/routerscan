32249b021cf74c142eb509c3f0f2ee85abdb8f1b
Gornostay <36516130+gornostay25@users.noreply.github.com>
2018-02-24 16:20:45 +0200

    Update rscan.py

--- rscan.py
+++ rscan.py
@@ -6,6 +6,7 @@ import sys
 import time as t
 try:
 	from netaddr import IPNetwork
+	import requests
 except ModuleNotFoundError:
 	print("будласка встановіть бібліотеки\npip install -r requirements.txt\n")
 	exit()
