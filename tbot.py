d9900cdc55e350a3bc803a6091a172998e0dda63
Gornostay <36516130+gornostay25@users.noreply.github.com>
2018-02-25 13:44:20 +0200

    Update tbot.py

--- tbot.py
+++ tbot.py
@@ -3,7 +3,7 @@ import requests
 
 
 url = ""
-def send_message(chat,text
-params = {'chat_id': chat, 'text': text}
+def send_message(chat,text):
+    params = {'cat_id': chat, 'text': text}
     response = requests.post(url + 'sendMessage', data=params)
     return response
