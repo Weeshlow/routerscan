050c55eb9fbfeba8ec80864c3318f7c91ed7faf6
gornostay <volodia.palamar250405@gmail.com>
2018-02-25 21:16:21 +0200

    telegram bot

--- tbot.py
+++ tbot.py
@@ -2,12 +2,11 @@
 import requests
 import sys
 def send_message(tocken,chat,text):
-    url = "https://api.telegram.org/{}/sendMessage?chat_id={}&text={}".format(tocken,chat,text)
-    response = requests.get(url)
-    return response
+	url = "https://api.telegram.org/{}/sendMessage?chat_id={}&text={}".format(tocken,chat,text)
+	response = requests.get(url)
+	return response
 
 if __name__ == '__main__':
-    if send_message(sys.argv[1],sys.argv[2],sys.argv[3]) == "<Response [200]>":
-        print("")
-       
-        
+	if send_message(sys.argv[1],sys.argv[2],sys.argv[3]) == "<Response [200]>":
+ 		print("")
+
