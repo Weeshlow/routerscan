b5e6bb6301ade46d9944db23f3388e07fcfc2c54
Gornostay <36516130+gornostay25@users.noreply.github.com>
2018-02-25 13:49:53 +0200

    Update tbot.py

--- tbot.py
+++ tbot.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
 import requests
-
-
-url = ""
+text = 
+tocken = 
+chatid = 
+url = "https://api.telegram.org/{}/sendmessage?chat_id={}&text={}".format(tocken,chatid,text)
 def send_message(chat,text):
     params = {'cat_id': chat, 'text': text}
     response = requests.post(url + 'sendMessage', data=params)
